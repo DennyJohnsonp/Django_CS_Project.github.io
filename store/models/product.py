2a2707730cb661419c7882b1a505820cb71526cf
DennyJohnsonP <dennyjohnsondj4@gmail.com>
2021-04-28 09:42:15 +0530

    Cart Page Started Commit

--- store/models/product.py
+++ store/models/product.py
@@ -5,8 +5,11 @@ class Product(models.Model):
     name = models.CharField(max_length=50) 
     price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
-    description = models.CharField(max_length=200,default='',null=True,blank=True)
+    description = models.CharField(max_length=100,default='',null=True,blank=True)
     image = models.ImageField(upload_to='uploads/produts/') 
+    
+    def get_product_by_id(ids):
+        return Product.objects.filter(id__in=ids)
     
     def __str__(self):
         return self.name
